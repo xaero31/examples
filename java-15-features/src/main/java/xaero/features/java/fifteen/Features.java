8ae9ef3fac2d5ec86254841b27580d800e6b380f
xaero <fontankrovi777@gmail.com>
2022-08-03 17:39:12 +0200

    add java 15 features

---
+++ java-15-features/src/main/java/xaero/features/java/fifteen/Features.java
@@ -0,0 +1,11 @@
+package xaero.features.java.fifteen;
+
+/**
+ * briefly view of some java 11 features
+ */
+public class Features {
+
+    public static void main(String[] args) {
+        System.out.println("don't have sufficient and not preview syntax features");
+    }
+}
