52bdc174517e0301d7a1226dbed2d25c7b9b35c3
xaero <fontankrovi777@gmail.com>
2022-08-31 16:45:58 +0200

    add some demo with concurrency

---
+++ concurrency/src/main/java/xaero/concurrency/demo/TimerDemo.java
@@ -0,0 +1,32 @@
+package xaero.concurrency.demo;
+
+import io.vavr.control.Try;
+import lombok.extern.java.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+@Log
+public class TimerDemo {
+
+    public static void main(String[] args) {
+        final var timer = new Timer();
+        // timer.schedule(new TimerDemoTask(), 0L, 2000L);
+        timer.scheduleAtFixedRate(new TimerDemoTask(), 0L, 2000L);
+        log.info("end");
+    }
+
+    @Log
+    private static class TimerDemoTask extends TimerTask {
+
+        @Override
+        public void run() {
+            Try.run(() -> {
+                log.info("start execute");
+                TimeUnit.SECONDS.sleep(10L);
+                log.info("end execute");
+            });
+        }
+    }
+}
