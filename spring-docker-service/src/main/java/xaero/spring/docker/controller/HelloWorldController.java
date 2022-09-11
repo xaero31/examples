6bc3a8fba918a0bd93eeb4f18e079ec8447c2196
xaero <fontankrovi777@gmail.com>
2022-09-12 01:15:38 +0200

    add docker application example

---
+++ spring-docker-service/src/main/java/xaero/spring/docker/controller/HelloWorldController.java
@@ -0,0 +1,35 @@
+package xaero.spring.docker.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static java.lang.System.getenv;
+import static java.nio.file.Files.writeString;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static org.springframework.http.ResponseEntity.ok;
+
+@Slf4j
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloWorld")
+    public ResponseEntity<String> helloWorld() {
+        return ok("hello world");
+    }
+
+    @GetMapping("/environment")
+    public ResponseEntity<String> environment() {
+        return ok(getenv("test.property"));
+    }
+
+    @GetMapping("/write")
+    public ResponseEntity<String> write() throws IOException {
+        writeString(Paths.get("/user-files/test.txt"), "text from application", CREATE);
+        return ok("file is written");
+    }
+}
